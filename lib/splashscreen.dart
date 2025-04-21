aa52f9ce644e8794d00d353ae440341c3a135588
Nur Anis Athirah <125522696+Anisfazed@users.noreply.github.com>
2025-04-21 14:01:23 +0800

    Create splashscreen.dart

---
+++ lib/splashscreen.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:randinfo/mainscreen.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(const Duration(seconds: 3), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => const MainScreen()),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 169, 250, 172), // soft light background
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              "assets/images/randifo.png",
+              height: 350,
+            ),
+            const SizedBox(height: 20),
+            const CircularProgressIndicator(
+              valueColor: AlwaysStoppedAnimation<Color>(Color.fromARGB(255, 0, 0, 0)), // soft green
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
